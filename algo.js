a386a5978d9bec374c1e3b98d94db25c0ba3c9f5
Long Vu <thienlong14th@gmail.com>
2021-03-09 13:11:02 -0600

    completed the assignment

---
+++ algo.js
@@ -0,0 +1,18 @@
+function oddFunc(){
+    for(var i=0; i<=20; i++){
+        if(i % 2 !== 0){
+            alert(i)
+    console.log(i);
+        }
+    }
+}
+
+function sumFunc(){
+    var sum = 0;
+    for(var i = 0; i <= 5; i++){
+        sum= sum + i
+        var num = i
+        alert("num: " + i,)
+        console.log("num: " + i, "sum: " +sum)
+    }
+}
